7e6e32a6b7f676eaa0f1fdd62e7e203367acded9
meeragarg705 <meeragarg705@gmail.com>
2020-08-04 11:58:00 +0530

    First Commit

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+define('CONSUMER_KEY', 'mUUnhtkRJhGtxO9ttJiQ8az3r'); // YOUR CONSUMER KEY
+define('CONSUMER_SECRET', 'ANqTqOf2GQ06D28EKKxFheDNyF7KmNZB9u1Uw4m0p77dXfQFKe'); //YOUR CONSUMER SECRET KEY 
+define('OAUTH_CALLBACK', 'http://99projects.in/twitterapi/index.php');  // Redirect URL 
+?>
